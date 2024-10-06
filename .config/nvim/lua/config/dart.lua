3f3276930fdf6511c51924884cc792baa320377b
Nick Steinbrenner <nssteinbrenner@gmail.com>
2024-10-06 14:22:56 -0700

    2024-10-06 - Update dotfiles

--- .config/nvim/lua/config/dart.lua
+++ .config/nvim/lua/config/dart.lua
@@ -13,7 +13,6 @@ local function increment_index(index)
     return index
 end
 
-
 local function decrement_index(index)
     if index > 1 then
         index = index - 1
@@ -32,15 +31,30 @@ local function set_mark(index, mark)
     vim.api.nvim_buf_set_mark(0, available_marks[index], mark[1], mark[2], {})
 end
 
-local function check_mark(mark)
-    return mark[1] ~= 0 and mark[0] ~= 0
+local function is_mark(mark)
+    return mark[1] ~= nil and mark[1] ~= 0
+end
+
+local function jump_to(index)
+    local end_of_file = vim.api.nvim_buf_line_count(0)
+    local dest_mark = get_mark(index)
+    if dest_mark[1] > end_of_file then
+        dest_mark[1] = end_of_file
+        set_mark(index, dest_mark)
+    end
+    if is_mark(dest_mark) then
+        vim.api.nvim_win_set_cursor(0, dest_mark)
+        vim.cmd.normal("zz")
+        return true
+    end
+    return false
 end
 
 local function add_mark()
     local location = vim.api.nvim_win_get_cursor(0)
     for i = 1, #available_marks do
         local found_mark = get_mark(i)
-        if found_mark[1] == 0 and found_mark[2] == 0 then
+        if is_mark(found_mark) == false then
             set_mark(i, location)
             current_index = i
             if oldest_index == nil then
@@ -59,34 +73,44 @@ local function add_mark()
     end
 end
 
-
 local function next_mark()
     current_index = increment_index(current_index)
 
-    local dest_mark = get_mark(current_index)
-    if check_mark(dest_mark) == false then
-        current_index = decrement_index(current_index)
-        return
+    for _ = 1, #available_marks do
+        local result = jump_to(current_index)
+        if result then
+            return
+        else
+            current_index = increment_index(current_index)
+        end
     end
-    vim.api.nvim_win_set_cursor(0, get_mark(current_index))
+    vim.print("No marks available.")
 end
 
 local function prev_mark()
     current_index = decrement_index(current_index)
 
-    local dest_mark = get_mark(current_index)
-    if check_mark(dest_mark) == false then
-        current_index = decrement_index(current_index)
-        return
+    for _ = 1, #available_marks do
+        local result = jump_to(current_index)
+        if result then
+            return
+        else
+            current_index = decrement_index(current_index)
+        end
     end
-    vim.api.nvim_win_set_cursor(0, get_mark(current_index))
+    vim.print("No marks available.")
 end
 
 vim.keymap.set("n", "<A-a>", add_mark)
-vim.keymap.set("n", "<A-t>", "'t")
-vim.keymap.set("n", "<A-h>", "'h")
-vim.keymap.set("n", "<A-n>", "'n")
-vim.keymap.set("n", "<A-s>", "'s")
+-- vim.keymap.set("n", "<A-t>", function() select(1) end)
+-- vim.keymap.set("n", "<A-h>", function() select(2) end)
+-- vim.keymap.set("n", "<A-n>", function() select(3) end)
+-- vim.keymap.set("n", "<A-s>", function() select(4) end)
+
+vim.keymap.set("n", "<A-t>", "'tzz")
+vim.keymap.set("n", "<A-h>", "'hzz")
+vim.keymap.set("n", "<A-n>", "'nzz")
+vim.keymap.set("n", "<A-s>", "'szz")
 
 vim.keymap.set("n", "<A-S-P>", prev_mark)
 vim.keymap.set("n", "<A-S-N>", next_mark)
